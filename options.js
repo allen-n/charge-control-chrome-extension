9c7a9c64eed3624ba5236308343798240ea4ca9f
allen-n <allennikka@gmail.com>
2020-05-24 19:22:10 -0700

    Able to store config variables in chrome storage.

--- options.js
+++ options.js
@@ -1,17 +1,24 @@
 // Saves options to chrome.storage
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
+    var maxCharge = document.getElementById('max-charge-percent').value
+    var minCharge = document.getElementById('min-charge-percent').value
+    var apiKey = document.getElementById('api-key').value
+    var onEventName = document.getElementById('on-event-name').value
+    var offEventName = document.getElementById('off-event-name').value
+
     chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
+        maxCharge: maxCharge,
+        minCharge: minCharge,
+        apiKey: apiKey,
+        onEventName: onEventName,
+        offEventName: offEventName
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
+        status.textContent = 'Options saved!';
         setTimeout(function () {
             status.textContent = '';
-        }, 750);
+        }, 1500);
     });
 }
 
@@ -20,14 +27,31 @@ function save_options() {
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
+        maxCharge: 80,
+        minCharge: 20,
+        apiKey: "",
+        onEventName: "",
+        offEventName: ""
     }, function (items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        document.getElementById('max-charge-percent').value = items.maxCharge
+        document.getElementById('min-charge-percent').value = items.minCharge
+        document.getElementById('api-key').value = items.apiKey
+        document.getElementById('on-event-name').value = items.onEventName
+        document.getElementById('off-event-name').value = items.offEventName
     });
     console.log("Options restored")
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
+document.getElementById('close').addEventListener('click', () => {
+    window.close();
+});
+document.getElementById('show-api-key').addEventListener('click', () => {
+    var x = document.getElementById("api-key");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+});
